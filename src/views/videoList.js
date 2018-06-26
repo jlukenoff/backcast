500aa4ffd542de5070781f86e15797635f4da467
jlukenoff <jlukenoff@gmail.com>
2018-06-25 17:31:11 -0700

    Implemented video model

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,9 +1,19 @@
 var VideoListView = Backbone.View.extend({
-
+  initialize: function() {
+    this.render();
+    this.collection.on('change', function() {
+      this.render();
+    }, this);
+  },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    
+    this.collection.forEach(function(model) {
+      debugger;
+      model.render();
+    });
     return this;
   },
 
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,5 +1,16 @@
 var VideoListEntryView = Backbone.View.extend({
-
+  initialize: function() {
+    this.render();
+    
+  },
+  
+  events: {
+    'click .video-list-entry-title': 'handleClick'
+  },
+  
+  handleClick: function() {
+    this.model.select();
+  },
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
