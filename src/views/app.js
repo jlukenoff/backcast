5b237ef47ec03a53e8ca8cc01d128dc20d8aa2a8
jlukenoff <jlukenoff@gmail.com>
2018-06-25 21:13:56 -0700

    passed multiple tests

--- src/views/app.js
+++ src/views/app.js
@@ -4,6 +4,8 @@ var AppView = Backbone.View.extend({
 
   initialize: function() {
     this.videos = new Videos();
+    this.videoPlayer = new VideoPlayerView().render();
+    this.videoList = new VideoListView();
     this.render();
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,19 +1,14 @@
 var VideoListView = Backbone.View.extend({
   initialize: function() {
     this.render();
-    this.collection.on('change', function() {
+    /*this.collection.on('change', function() {
       this.render();
-    }, this);
+    }, this);*/
   },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
-    
-    this.collection.forEach(function(model) {
-      debugger;
-      model.render();
-    });
     return this;
   },
 
